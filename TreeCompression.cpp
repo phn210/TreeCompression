928f4c14187adac57b9ef864199181b9e6f6015b
phn210 <57532941+phn210@users.noreply.github.com>
2019-11-08 21:22:55 +0700

    Create TreeCompression.cpp

---
+++ TreeCompression.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+struct CANH
+    {
+        int dinhTren;
+        int dinhDuoi;
+    };
+CANH* canh;
+
+int timKiem(int x, CANH* canh, int n)
+{
+    int i,tontai=0;
+    for(i=0;i<n;i++)
+    {
+        if ((canh[i].dinhTren==x&&canh[i].dinhTren!=0)||(canh[i].dinhDuoi==x&&canh[i].dinhDuoi!=0))
+        {
+            tontai=1; break;
+        }
+    }
+    if (tontai) return i;
+    else return -1;
+}
+
+int main()
+{
+    int n,i=0;
+    cout << "Nhap so canh: ";
+    cin >> n;
+    CANH* canh;
+    canh = (CANH*)calloc(n,sizeof(CANH));
+    for(i=0;i<n;i++)
+    {
+        cout << "Nhap canh thu " << i+1 << endl;
+        cin >> canh[i].dinhTren >> canh[i].dinhDuoi;
+    }
+    int bac[1001];
+    for(i=0;i<=n;i++) bac[i]=0;
+    for(i=0;i<n;i++)
+    {
+        bac[canh[i].dinhTren]++;
+        bac[canh[i].dinhDuoi]++;
+    }
+    CANH* prufer;
+    prufer = (CANH*)calloc(n,sizeof(CANH));
+    int k; // k la vi tri cua dinh bac 1 nho nhat
+    int j=0;
+    for(i=0;i<=n;i++)
+    {
+        if (bac[i]==1&&i!=0)
+        {
+            k=timKiem(i,canh,n);
+            if (k!=-1)
+            {
+                if (bac[canh[k].dinhTren]>=bac[canh[k].dinhDuoi])
+                {
+                    prufer[j].dinhTren=canh[k].dinhDuoi;
+                    prufer[j].dinhDuoi=canh[k].dinhTren;
+                }
+                else
+                {
+                    prufer[j].dinhTren=canh[k].dinhTren;
+                    prufer[j].dinhDuoi=canh[k].dinhDuoi;
+                }
+                bac[canh[k].dinhDuoi]--;
+                bac[canh[k].dinhTren]--;
+                bac[0]=bac[0]+2;
+                j++;
+                canh[k].dinhTren=0;
+                canh[k].dinhDuoi=0;
+                i=0;
+            }
+        }
+    }
+    cout << "Prufer code cua cay la: ";
+    for (i=0;i<n-1;i++)
+    {
+        cout << prufer[i].dinhDuoi << " ";
+    }
+    free(canh); free(prufer);
+    return 0;
+}
